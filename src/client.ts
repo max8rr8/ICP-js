190059445f0becc76a95002a71531b2b6a1ea948
maximmasterr <mrom06@ya.ru>
2020-01-23 15:48:57 +0300

    Added promise to send function

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,19 @@
 import { ReplyCallback, RequestMessage, ReplyMessage } from './types';
 
 export class ICPConnection {
-  protected handlers: Map<number, any> = new Map();
-  public send(msg: RequestMessage, callback: ReplyCallback) {
-    const id = Math.random() * 0xffffff;
-    msg.id = id;
-    this.handlers.set(id, callback);
-    this.__write(JSON.stringify(msg));
+  protected handlers: Map<number, (msg: ReplyMessage) => void> = new Map();
+
+  public send(msg: RequestMessage): Promise<ReplyMessage>;
+  public send(msg: RequestMessage, callback: ReplyCallback): void;
+  public send(msg: RequestMessage, callback?: ReplyCallback): Promise<ReplyMessage> | void {
+    const promise: Promise<ReplyMessage> = new Promise(resolve => {
+      const id = Math.random() * 0xffffff;
+      msg.id = id;
+      this.handlers.set(id, out => resolve(out));
+      this.__write(JSON.stringify(msg));
+    });
+    if (!callback) return promise;
+    else promise.then(callback);
   }
 
   protected __write(msg: string) {
@@ -17,7 +24,7 @@ export class ICPConnection {
 
   protected handle(msg: ReplyMessage) {
     if (this.handlers.has(msg.id)) {
-      this.handlers.get(msg.id)(msg);
+      (this.handlers.get(msg.id) ?? (() => 0))(msg);
       this.handlers.delete(msg.id);
     }
   }
